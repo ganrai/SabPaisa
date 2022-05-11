aec04508f615d62a52b3b5badc86e2bbecc588a5
Your Name <you@example.com>
2022-05-11 16:35:41 +0530

    cordova web view code added

--- www/SabPaisa.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-module.exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'SabPaisa', 'coolMethod', [arg0]);
-};
-
-module.exports.makePayment = function(arg0,success,error){
-    exec(success, error, 'SabPaisa','makePayment',[arg0]);
-}
